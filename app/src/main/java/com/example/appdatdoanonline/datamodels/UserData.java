2558c384f24e917b942c541032546bad0498c6b9
Admin <hokhacdang2001@gmail.com>
2021-11-18 20:58:31 +0700

    Data models UserData cho nhóm để tiếp tục làm phần của mình

---
+++ app/src/main/java/com/example/appdatdoanonline/datamodels/UserData.java
@@ -0,0 +1,75 @@
+package com.example.appdatdoanonline.datamodels;
+
+public class UserData {
+    private String sUserName, sFullName, sSdt, sGioiTinh, sDiaChi, sPassword;
+    private int iPermission;
+    public UserData() {
+
+    }
+
+    public UserData(String sUserName, String sFullName, String sSdt, String sGioiTinh, String sDiaChi, String sPassword, int iPermission) {
+        this.sUserName = sUserName;
+        this.sFullName = sFullName;
+        this.sSdt = sSdt;
+        this.sGioiTinh = sGioiTinh;
+        this.sDiaChi = sDiaChi;
+        this.sPassword = sPassword;
+        this.iPermission = iPermission;
+    }
+
+    public String getsUserName() {
+        return sUserName;
+    }
+
+    public void setsUserName(String sUserName) {
+        this.sUserName = sUserName;
+    }
+
+    public String getsFullName() {
+        return sFullName;
+    }
+
+    public void setsFullName(String sFullName) {
+        this.sFullName = sFullName;
+    }
+
+    public String getsSdt() {
+        return sSdt;
+    }
+
+    public void setsSdt(String sSdt) {
+        this.sSdt = sSdt;
+    }
+
+    public String getsGioiTinh() {
+        return sGioiTinh;
+    }
+
+    public void setsGioiTinh(String sGioiTinh) {
+        this.sGioiTinh = sGioiTinh;
+    }
+
+    public String getsDiaChi() {
+        return sDiaChi;
+    }
+
+    public void setsDiaChi(String sDiaChi) {
+        this.sDiaChi = sDiaChi;
+    }
+
+    public String getsPassword() {
+        return sPassword;
+    }
+
+    public void setsPassword(String sPassword) {
+        this.sPassword = sPassword;
+    }
+
+    public int getiPermission() {
+        return iPermission;
+    }
+
+    public void setiPermission(int iPermission) {
+        this.iPermission = iPermission;
+    }
+}
